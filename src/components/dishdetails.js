ab23007d0513107d4e5208c84c9eabd88ef5ba66
amal shyjo <amalshyjo@gmail.com>
2020-06-13 15:36:04 +0530

    Before 1st assignment

---
+++ src/components/dishdetails.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import {Card,CardImg,CardText,CardTitle,CardBody} from "reactstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+class Dishdetail extends Component {
+
+    render() { 
+        const {dish} = this.props;
+        return ( 
+            <div className="row">
+
+            {this.renderDish(dish)}
+
+            </div>
+        );
+    }
+
+
+    renderDish=(dish)=>
+    {
+        if (dish!=null)
+        {
+            return(
+            <React.Fragment>
+            <div className="col-12 col-md-5 m-1">
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+            </div>
+            <div className="col-12 col-md-5 m-1" >
+            <h4>Comments</h4>
+            {this.renderComments(dish.comments)}
+            </div>
+            </React.Fragment>
+            )
+        }
+        else{
+            return(<div></div>)
+        }
+    }
+
+    renderComments =(comments) =>
+    {
+
+        if (comments!=null)
+        {
+            const com = comments.map(commts=>{
+
+                    return(
+                    <ul className="list-unstyled">
+                    <li>{commts.comment}</li><br />
+                    <li>-- {commts.author}, {this.commts.date}</li><br />
+                    </ul>
+                )
+
+                }
+                );
+        }
+        else{
+            return(<div></div>)
+        }
+    }
+}
+
+
+
+export default Dishdetail;
